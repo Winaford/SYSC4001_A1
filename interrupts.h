cfa59d063a3d665b0f7d06f0f19b39404df1fdec
Winaford <113386334+Winaford@users.noreply.github.com>
2025-10-06 03:45:14 -0400

    Update interrupts.h
    Removed old functions

--- interrupts.h
+++ interrupts.h
@@ -9,16 +9,13 @@
 int find_vector_index(int device_code);
 unsigned int get_isr_address(int index);
 
-// Looks up a vector table entry by interrupt number
-struct VectorTableEntry* lookup_vector(int interruptNumber);
-
-// Handles CPU execution with 5 actions
+// Handles CPU execution
 void handle_cpu(int value, int *current_time, FILE *output_file);
 
-// Handles SYSCALL event using the vector table
+// Handles SYSCALL event
 void handle_syscall(int vector_index, int value, int *current_time, FILE *output_file);
 
-// Handles END_IO event using the vector table
+// Handles END_IO event
 void handle_end_io(int vector_index, int value, int *current_time, FILE *output_file);
 
 // Processes the trace file and writes to the output file
